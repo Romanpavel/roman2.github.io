b11710548d4a6a748beb12a71fee82ceed9c9f54
Ubuntu <ubuntu@ip-172-31-56-102.ec2.internal>
2020-06-24 18:01:13 +0000

    lodown update

--- projects/eloquent-javascript/chapter-04/exercises.js
+++ projects/eloquent-javascript/chapter-04/exercises.js
@@ -2,25 +2,59 @@
 // range ///////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+// create a range function, with parameters of start, stop, and step
+// assign the default step parameter to a ternary operator,
+// if start is less than end, step is assigned to 1, -1 if its the opposite
+
 function range(start, end, step = start < end ? 1 : -1) {
+  
+  // create an array to hold the values 
   let array = [];
+  
+  // if the start and end numbers are the same return an empty array
+  
+  if (start === end) {
+    return array;
+  }
+
+// if start is less than end, step would be 1, print all numbers between start and end, incrementing by 1
 
   if (step > 0) {
-    for (let i = start; i <= end; i += step) array.push(i);
+    for (let i = start; i <= end; i += step) {
+      array.push(i);
+    }
+    
+    // else end is greater than start, create a range going in reverse
   } else {
-    for (let i = start; i >= end; i += step) array.push(i);
+    for (let i = start; i >= end; i += step) {
+      array.push(i);
+    }
   }
+  
+  // return the range
+  
   return array;
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 // sum /////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 function sum(array) {
+  
+  // create a variable to hold the sum of values and assign it to total
+  
   let total = 0;
+  
+  // loop through all elements of array with a for of loop
   for (let value of array) {
+    
+    // re-asign all values to total, adding them one by one
     total += value;
   }
+  
+  // return the total
+  
   return total;
 }
 
@@ -29,10 +63,21 @@ function sum(array) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function reverseArray(array) {
+  
+  // create a place to hold the reversed array
+  
   let output = [];
+  
+  // start the loop at the back of the array and stop it including 0, decrement every time
   for (let i = array.length - 1; i >= 0; i--) {
+    
+    // add the value at every index going backwards to the output array
+    
     output.push(array[i]);
   }
+  
+  // return the reversed array
+  
   return output;
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,11 +85,27 @@ function reverseArray(array) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function reverseArrayInPlace(array) {
+  
+  // create a for loop starting at the first index and ending in the middle, increment by 1
+  // use math.floor in case the there is an odd number of elements in the array
+  
   for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    
+    // assign a variable to all the elements 
+    
     let old = array[i];
+    
+    // re-assign the elements to the last elemement in the array, counting backwards
+    
     array[i] = array[array.length - 1 - i];
+    
+    // re-assign that element to the old variable
+    
     array[array.length - 1 - i] = old;
   }
+  
+  // return the array
+  
   return array;
 }
 
@@ -53,8 +114,16 @@ function reverseArrayInPlace(array) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function arrayToList(array) {
+  
+  // assign list to the null dataType
+  
   let list = null;
+  
+  // loop over the array in reverse
+  
   for (let i = array.length - 1; i >= 0; i--) {
+    
+    // re-assign list to an object, with keys of value: element, and rest: the list
     list = {value: array[i], rest: list};
   }
   return list;
@@ -65,8 +134,15 @@ function arrayToList(array) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function listToArray(list) {
+  
+  // create an empty array to hold the values
+  
   let array = [];
+  
+  // loop over the list
   for (let node = list; node; node = node.rest) {
+    
+    // add object value to the array
     array.push(node.value);
   }
   return array;
@@ -76,6 +152,9 @@ function listToArray(list) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function prepend(value, list) {
+  
+  // reutrn the object, with the keyword rest
+  
   return {value, rest: list};
 }
 
@@ -84,27 +163,62 @@ function prepend(value, list) {
 ////////////////////////////////////////////////////////////////////////////////
 
 function nth(list, n) {
-  if (!list) return undefined;
-  else if (n == 0) return list.value;
-  else return nth(list.rest, n - 1);
+  
+  // if the list doesnt exst, return undefined
+  if (!list) {
+    return undefined;
+  }
+  // if n is 0 reurn the value at the value key 
+  else if (n == 0) {
+    return list.value;
+  }
+  
+  // using recuresion decrease n until you reach the base case
+  else {
+    return nth(list.rest, n - 1);
+  }
 }
 ////////////////////////////////////////////////////////////////////////////////
 // deepEqual ///////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 function deepEqual(a, b) {
-  if (a === b) return true;
   
-  if (a == null || typeof a != "object" ||
-      b == null || typeof b != "object") return false;
+  // if a is equal to b return true
+  
+  if (a === b) {
+    return true;
+  }
+  
+  // if a or b is null, or a or b is not an object, return false
+  
+  if (a == null || typeof a != "object" || b == null || typeof b != "object") {
+    return false;
+  }
+  
+  // if a and b are objects, get all keys from them
 
-  let keysA = Object.keys(a), keysB = Object.keys(b);
+  let keysA = Object.keys(a);
+  let keysB = Object.keys(b);
+  
+  // if the amount of keys are not the same, return false
 
-  if (keysA.length != keysB.length) return false;
+  if (keysA.length != keysB.length) {
+    return false;
+  }
+  
+  // loop through the keysA array
 
   for (let key of keysA) {
-    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) return false;
+    
+    // if any keys of keyA are not in the keysB array, or the values of a and b do not striclty equal, return false
+    
+    if (!keysB.includes(key) || !deepEqual(a[key], b[key])) {
+      return false;
+    }
   }
+
+// otherwise return true
 
   return true;
 }
--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -2,7 +2,11 @@
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function flatten() {
+function flatten(array) {
+  
+  // reduce the array by concatting the the current element to the accumulator
+  
+  return array.reduce((acc, curr) => acc.concat(curr), []);
 
 }
 
@@ -10,26 +14,71 @@ function flatten() {
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function loop() {
-
+function loop(start, test, update, body) {
+  
+  // starting condition at the starting parameter
+  // stop at the test function of the indexed values
+  // re-asign value to the update function passing in the value
+  
+  for (let value = start; test(value); value = update(value)) {
+    
+    // update the the body through every iteration of value
+    body(value);
+  }
 }
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function every() {
-
+function every(array, predicate) {
+  
+  // loop over the array
+  
+  for (let element of array) {
+    
+    // if the result of passing all elements through predicate function does not exist, return false
+    if (!predicate(element)) {
+      return false;
+    }
+  }
+  // otherwise return true
+  return true;
 }
+
+
 
 // /////////////////////////////////////////////////////////////////////////////
 // dominantDirection ///////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
 
-function dominantDirection() {
+function dominantDirection(text) {
+  
+  // assign the counted variable to a function with parameters of text and char
+  
+  let counted = countBy(text, char => {
+    
+    // assign variable script to the codePoint of the character at the 1st index
+    
+    let script = characterScript(char.codePointAt(0));
+    
+    // if script exists return the directional object, return string 'none' if it doenst 
+    
+    return script ? script.direction : "none";
+    
+    // chain on filter, return names not assigned to 'none'
+    
+  }).filter(({name}) => name != "none");
+  
+  // if the there are no elements in counted, return the string 'ltr'
 
+  if (counted.length === 0) {
+    return "ltr";
 }
 
+// return the name, using reduce, to find wich count is larger
+  return counted.reduce((a, b) => a.count > b.count ? a : b).name;
+}
 // /////////////////////////////////////////////////////////////////////////////
 //  //////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
